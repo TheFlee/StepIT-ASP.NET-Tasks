b03eaffaa131767ea6ac98c9e47e00d5192b6691
Firidun Hasanli <firidunhesenli112@gmail.com>
2026-01-08 11:57:16 +0400

    Add project files.

---
+++ 01. MVC Example/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace _01._MVC_Example.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
